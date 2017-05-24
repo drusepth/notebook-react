65596a41a81b900ebc0ca6ef0c40c3277b45c030
Andrew Brown <drusepth@gmail.com>
2017-05-24 01:08:26 +0000

    es6

--- app/notebook.js
+++ app/notebook.js
@@ -1,10 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-// var App = require('./components/App');
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// ReactDOM.render(
-//   <App />,
-//   document.getElementById('app')
-// );
+// import App from './components/App';
 
-document.write('hello');
+// ReactDOM.render(<App />, document.getElementById('app'));
