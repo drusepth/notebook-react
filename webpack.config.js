2d128f5b6c27827d804f0b12235b35656353c460
Andrew Brown <drusepth@gmail.com>
2017-05-24 00:24:42 +0000

    React setup with node

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var HTMLWebpackPlugin = require('html-webpack-plugin');
+var HTMLWebpackPluginConfig = new HTMLWebpackPlugin({
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+module.exports = {
+  entry: './app/notebook.js',
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'notebook.min.js',
+    path: __dirname + '/build'
+  },
+  plugins: [HTMLWebpackPluginConfig]
+};
