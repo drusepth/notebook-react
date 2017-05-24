f9162d3ff339d55efd1d0000fbc964399198ef2a
Andrew Brown <drusepth@gmail.com>
2017-05-24 14:47:05 +0000

    boy howdy is that some material design

--- app/notebook.js
+++ app/notebook.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyComponent from '../components/MyComponent';
 
 // import App from './components/App';
 
 // ReactDOM.render(<App />, document.getElementById('app'));
+
+const App = () => (
+  <MuiThemeProvider>
+    <MyComponent />
+  </MuiThemeProvider>
+);
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
